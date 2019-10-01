3ecd4e90727aab0dd774b484a9375dbf0f0415bb
Joy Lin <vvlnote@hotmail.com>
2019-10-01 22:55:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,32 @@
 def starts_with_a_vowel?(word)
-
+  matched = ""
+  matched = word.match(/^[aeiou]/i)
+  
+  matched == nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  words = []
+  words = text.scan(/un+\w+ing/)
+  
+  words
 end
 
 def words_five_letters_long(text)
+  words = []
 
+  text.scan(/\w{5,}/).each do |word|
+    if word.length == 5 
+      words << word
+    end
+  end
+  words
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  word = nil
+  word = text.match(/[A-Z]\w+[,.!;:]/)
+  word != nil ? true : false
 end
 
 def valid_phone_number?(phone)
