82736a4ef3d3ad53625b386894ad120424b94ad3
Joy Lin <vvlnote@hotmail.com>
2019-10-01 22:58:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,14 +1,14 @@
 def starts_with_a_vowel?(word)
   matched = ""
   matched = word.match(/^[aeiou]/i)
-  
+
   matched == nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
   words = []
   words = text.scan(/un+\w+ing/)
-  
+
   words
 end
 
@@ -16,7 +16,7 @@ def words_five_letters_long(text)
   words = []
 
   text.scan(/\w{5,}/).each do |word|
-    if word.length == 5 
+    if word.length == 5
       words << word
     end
   end
@@ -30,5 +30,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/([0-9]{3}\)|(([0-9]{3}-[0-9]{4})|([0-9] *?){10}|[0-9]{7})\b)/) != nil
 end
